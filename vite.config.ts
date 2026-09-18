ce615121f86c64646ba6f4c9dd2b065ad9f930bc
Fatih Can <cfatih.can@gmail.com>
2026-09-19 01:18:23 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 import path from "node:path";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    legacy({
+      // Eski iPhone/Safari sürümlerini hedefliyoruz
+      targets: ['iOS >= 12', 'Safari >= 12', 'defaults'],
+      renderLegacyChunks: true,
+    }),
   ],
   resolve: {
     alias: {
